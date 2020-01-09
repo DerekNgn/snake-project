bc981b3f5cf882473870ce99797191497a4cd7b3
DerekNgn <nguyen.keymon@gmail.com>
2020-01-08 21:48:56 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Snake Game</title>
+    </head>
+    <link rel=stylesheet type="text/css" href="css/style.css">
+    <script src="js/app.js"></script>
+    <body>
+        <h1>Score</h1>
+        <h2 id="score">0</h2>
+        <canvas id="gameArea" width="600" height="600"></canvas>
+    </body>
+</html>
