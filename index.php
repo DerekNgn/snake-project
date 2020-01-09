92aadb72a79c608c1633afc5ddf0d0d240a1a4b6
DerekNgn <nguyen.keymon@gmail.com>
2020-01-08 21:54:36 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,8 @@
     <link rel=stylesheet type="text/css" href="css/style.css">
     <script src="js/app.js"></script>
     <body>
-    <?php 
-        echo "<h1>Score</h1>
+        <h1>Score</h1>
         <h2 id="score">0</h2>
-        <canvas id="gameArea" width="600" height="600"></canvas>";
-        ?>
+        <canvas id="gameArea" width="600" height="600"></canvas>"
     </body>
 </html>
